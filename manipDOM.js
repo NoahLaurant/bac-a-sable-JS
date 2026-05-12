f77dbcacebe741cf471f53333c9b8a110e68e040
NoahLaurant <101389192+NoahLaurant@users.noreply.github.com>
2026-05-12 16:37:49 +0200

    maj

--- manipDOM.js
+++ manipDOM.js
@@ -1,10 +1,6 @@
+/*
+
 //trois manières de séléctionner des éléments dans mon document c à d dans ma feuil
-
-
-
-
-
-
 
 //séléctionner le body et ajouter une div
 
@@ -22,3 +18,73 @@ function ajouteUnParagraphe() {
     document.querySelector
 }
 `
+*/
+
+//Exo 1
+
+function bleu (){
+    let h1 = document.querySelector("h1");
+    h1.style.color = "blue";
+    
+ 
+}
+
+
+
+//Exo 2
+
+function cacher(){
+    let h2 = document.querySelector("#cache");
+   h2.classList.toggle("none")
+}
+
+
+//Exo 3
+
+    let divCarre = document.querySelectorAll(".border")
+
+
+    //je parcours mon tableau de div
+    divCarre.forEach(border => {
+        border.addEvenListener("click",function(){
+
+            //je reset
+            reset()
+            //j'ai la boite sur laquelle j'ai cliqué
+            console.log(border)
+            border.classList.toggle("remplir")
+        })
+    });
+
+    //role : reset(enleve la class border a toutes les div qui ont la classe boite)
+    //parametre : non
+    //return : non
+
+    function reset() {
+       let Carre =  document.querySelectorAll(".border")
+       Carre.forEach(carre => {
+        carre.classList.remove("remplir")
+       })
+    }
+
+
+
+
+    //Exo 4
+    let nombre = 0;
+    let bouton = document.querySelector("btn2");
+    let compteur = document.querySelector("#compteur");
+
+    compteur.addEventListener("click",function(){
+        nombre++;
+        compteur.innerHTML = nombre;
+    })
+
+
+
+    //Exo 5
+    
+
+
+
+    
